0803ba9eb1b0fdc4c407d4aff8df641c057ccce1
Ludger Solbach <soulman@soulspace.org>
2023-03-17 22:23:29 +0100

    refactorings, moved time namespace

--- src/org/soulspace/clj/astronomy/time.clj
+++ src/org/soulspace/clj/astronomy/time.clj
@@ -9,7 +9,7 @@
 ;;;;
 ;;;;   You must not remove this notice, or any other, from this software.
 ;;;;
-(ns org.soulspace.clj.astronomy.time.time
+(ns org.soulspace.clj.astronomy.time
   (:require [org.soulspace.math.core :as m])
   (:import [java.util Date GregorianCalendar SimpleTimeZone TimeZone]))
 
@@ -338,3 +338,22 @@
            (* 0.000387933M T T)
            (* -1 (/ (m/pow T 3) 38710000M)))
          360)))
+
+
+(defprotocol Instant
+  (as-julian-day [date] "Returns the julian day of this point in time.")
+  (as-date [date]))
+
+(defrecord JulianDay [jd timezone]
+  Instant
+  (as-julian-day [this] jd)
+  (as-date [this] (julian-day-to-date jd)))
+
+(defn new-julian-day
+  "Creates a new julian day instant."
+  ([]
+   (new-julian-day (Date.) (TimeZone/getDefault)))
+  ([date]
+   (new-julian-day date (TimeZone/getDefault)))
+  ([date timezone]
+   (JulianDay. (java-date-to-julian-day date) timezone)))
