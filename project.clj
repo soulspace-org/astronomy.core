3319b3529716718919ad5499da3546cf2cab93a3
Ludger Solbach <soulman@soulspace.org>
2023-03-18 11:21:31 +0100

    updated project.clj

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "https://github.com/lsolbach/CljAstronomy"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.soulspace.clj/clj.base "0.8.1"]
-                 [org.soulspace.clj/math.core "0.8.0"]]
-  :test-paths ["test"])
+
+  ; use deps.edn dependencies
+  :plugins [[lein-tools-deps "0.4.5"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+
+  :test-paths ["test"]
+  :scm {:name "git" :url "https://github.com/soulspace-org/astronomy.core"}
+  :deploy-repositories [["clojars"  {:sign-releases false :url "https://clojars.org/repo"}]])
