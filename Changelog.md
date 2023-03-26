a481dc51149099bd8fcef16857c61675146b9276
Ludger Solbach <soulman@soulspace.org>
2023-03-26 17:28:18 +0200

    released version 0.4.0

--- Changelog.md
+++ Changelog.md
@@ -2,8 +2,8 @@ Changelog
 
 Version 0.4.0
 -------------
-
 * Dependency updates
 * Refactorings
 * Bug fixes
 * Comment cleanups
+
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.soulspace.clj/astronomy.core "0.4.0-SNAPSHOT"
+(defproject org.soulspace.clj/astronomy.core "0.4.0"
   :description "A library of astronomical algorithms in clojure"
   :url "https://github.com/soulspace-org/astronomy.core"
   :license {:name "Eclipse Public License"
