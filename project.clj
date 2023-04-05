31ba6ee05a72f9daa2906dea77a000ae00e0c006
Ludger Solbach <soulman@soulspace.org>
2023-04-05 17:35:05 +0200

    added warn-on-reflection to dev profile

--- project.clj
+++ project.clj
@@ -8,11 +8,15 @@
 ;  :plugins [[lein-tools-deps "0.4.5"]]
 ;  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
 ;  :lein-tools-deps/config {:config-files [:install :user :project]}
-  
+
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.soulspace.clj/clj.base "0.9.0"]
                  [org.soulspace.clj/math.core "0.9.0"]]
 
   :test-paths ["test"]
+
+  :profiles {:dev {:dependencies [[djblue/portal "0.37.1"]]
+                   :global-vars {*warn-on-reflection* true}}}
+
   :scm {:name "git" :url "https://github.com/soulspace-org/astronomy.core"}
   :deploy-repositories [["clojars"  {:sign-releases false :url "https://clojars.org/repo"}]])
